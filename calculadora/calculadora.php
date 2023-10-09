84c8ecbc8cb0d45fb369bfd19bf10a461c1ffa42
adri181 <adriananascimentoalves6@gmail.com>
2023-10-09 08:18:46 -0300

    init

---
+++ calculadora/calculadora.php
@@ -0,0 +1,29 @@
+<h1>Resultado da Tabuada</h1>
+
+<?php
+if (isset($_GET["numero"])) 
+{
+    $numero = intval($_GET["numero"]);
+
+    if (is_numeric($numero)) 
+    {
+        echo "<p>Tabuada do $numero:</p>";
+        $i = 0;
+
+        while ($i <= 10) 
+        {
+            $resultado = $numero * $i;
+            echo "<p>$numero x $i = $resultado</p>";
+            $i++;
+        }
+
+    } 
+    
+    else 
+    {
+        echo "<p>Por favor, insira um número válido.</p>";
+    }
+}
+?>
+
+<p><a href="calculadora.html">Calcular outra tabuada</a></p>
---
+++ calculadora/financeiro.php
@@ -0,0 +1,29 @@
+<?php
+
+    if (isset($_GET["valor_investimento"])) 
+    {
+        $valor_investimento = floatval($_GET["valor_investimento"]);
+        
+        if (is_numeric($valor_investimento) && $valor_investimento >= 0) 
+        {
+            echo "<p>Depósito inicial: " . number_format($valor_investimento, 2, ',', '.') . "</p>";
+            $dia = 1;
+            
+            while ($dia <= 30) 
+            {
+                $valor_investimento *= (1 + 0.00008); 
+                echo "<p>Dia $dia - " . number_format($valor_investimento, 2, ',', '.') . "</p>";
+                $dia++;
+            }
+
+        } 
+
+        else 
+        {
+            echo "<p>Por favor, insira um valor de investimento válido.</p>";
+        }
+
+    }
+    ?>
+
+    <p><a href="financeiro.html">Calcular outro investimento</a></p>
